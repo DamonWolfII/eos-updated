29a64678749a4166df0db5b08eef4d47b7068aea
Nishant Adhikari <utsav2071@outlook.com>
2024-07-08 08:52:16 +0545

    Map update

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
@@ -16,33 +16,75 @@ const defaultIcon = L.icon({
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
     shadowSize: [41, 41],
-  });
+});
 
 const Map = () => {
-  return (
-    
-        <div className="w-full  flex justify-center bg-white h-96">
+    const mapRef = useRef<L.Map>(null);
+    const [showOverlay, setShowOverlay] = useState(false);
+
+    useEffect(() => {
+        const handleWheel = (e: WheelEvent) => {
+            const map = mapRef.current;
+            if (map) {
+                if (e.ctrlKey) {
+                    e.preventDefault();
+                    setShowOverlay(false);
+                    if (e.deltaY < 0) {
+                        map.zoomIn();
+                    } else {
+                        map.zoomOut();
+                    }
+                } else {
+                    setShowOverlay(true);
+                }
+            }
+        };
+
+        const handleKeyDown = (e: KeyboardEvent) => {
+            if (e.key === "Control") {
+                setShowOverlay(false);
+            }
+        };
+
+        document.addEventListener('wheel', handleWheel, { passive: false });
+        document.addEventListener('keydown', handleKeyDown);
+        document.addEventListener('keyup', handleKeyDown);
+
+        return () => {
+            document.removeEventListener('wheel', handleWheel);
+            document.removeEventListener('keydown', handleKeyDown);
+            document.removeEventListener('keyup', handleKeyDown);
+        };
+    }, []);
+
+    return (
+        <div className="w-full flex justify-center bg-white h-96 relative">
+            {showOverlay && (
+                <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center z-20">
+                    <p className="text-white text-xl font-semibold">Use Ctrl + Scroll to Zoom the map</p>
+                </div>
+            )}
             <MapContainer
                 className="h-full w-full z-10"
                 center={[27.715186, 85.369466]}
                 zoom={17}
-                scrollWheelZoom={true}
+                scrollWheelZoom={false}
+                ref={mapRef}
             >
-            <TileLayer
-                attribution=" "
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[27.715186, 85.369466]} icon={defaultIcon}>
-                <Popup>
-                <div>
-                    <p>EOS Web Solution</p>
-                </div>
-                </Popup>
-            </Marker>
+                <TileLayer
+                    attribution=" "
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[27.715186, 85.369466]} icon={defaultIcon}>
+                    <Popup>
+                        <div>
+                            <p>EOS Web Solution</p>
+                        </div>
+                    </Popup>
+                </Marker>
             </MapContainer>
         </div>
-    
-  )
-}
+    );
+};
 
-export default Map
+export default Map;
